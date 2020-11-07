62b12447de5ee3030add896af7533c165b06e293
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-11-06 19:50:34 -0700

    working on script for animation state machine

---
+++ DGM2210Fall2020/DGM 2210 Fall 2020/Assets/Anims/StateBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class StateBehaviour : MonoBehaviour
+{
+    private CharacterController controller;
+    public float speed = 5;
+    private Vector3 position = Vector3.zero;
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        var direction = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        position.Set(direction, 0,0 );
+        controller.Move(position);
+        //if left key do backwardWalk
+        //if right arrow do forwardWalk
+        //if none, do idle
+    }
+}
